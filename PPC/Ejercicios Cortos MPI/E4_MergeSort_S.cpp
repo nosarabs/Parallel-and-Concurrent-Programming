098af728e566c7c9c9c634509a44d5a66213f636
Berta <nosarabs@gmail.com>
2018-11-26 13:50:38 -0600

    Update PPC/Ejercicios Cortos MPI/E4_MergeSort_S.cpp

--- PPC/Ejercicios Cortos MPI/E4_MergeSort_S.cpp
+++ PPC/Ejercicios Cortos MPI/E4_MergeSort_S.cpp
@@ -27,18 +27,18 @@ using namespace std;
 
 void uso(string nombre_prog);
 void genNums(int* vec, int n, int mid);
-void print(int* vec, int mid);
+void print(int* vec, int mid, int tam);
 void mergeSort(int* vec, int mid, int cantxProc);
 void mergeParalelo(int* vec, int mid);
 
-int tam = 10;
 int cantxProc;
 
 void obt_args(
-	char*    argv[]        /* in  */,
-	int&     dato_salida  /* out */);
+	char*    argv[]        /* in  */, int& tam);
 
 int main(int argc, char* argv[]) {
+	double local_start, local_finish, local_elapsed, elapsed;
+	int tam;
 	int mid; // id de cada proceso
 	int cnt_proc; // cantidad de procesos
 	MPI_Status mpi_status; // para capturar estado al finalizar invocación de funciones MPI
@@ -54,7 +54,11 @@ int main(int argc, char* argv[]) {
 	MPI_Barrier(MPI_COMM_WORLD);
 #  endif
 
+	obt_args(argv, tam);
+
 	/* ejecución del proceso principal */
+	local_start = MPI_Wtime();
+
 	cantxProc = tam / cnt_proc;
 	int* local = new int[tam];
 	int* total = new int[tam];
@@ -70,12 +74,18 @@ int main(int argc, char* argv[]) {
 	MPI_Barrier(MPI_COMM_WORLD); // para sincronizar la finalización de los procesos
 	MPI_Reduce(local, total, tam, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+	local_finish = MPI_Wtime();
+	local_elapsed = local_finish - local_start;
+	MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
 	if (mid == 0) {
 
 		for (int i = 0; i < cnt_proc - 1; ++i) {
-			inplace_merge(total, total+(cantxProc * (i + 2)) - cantxProc, total + (cantxProc * (i + 2)));
+			inplace_merge(total, total + (cantxProc * (i + 2)) - cantxProc, total + (cantxProc * (i + 2)));
 		}
-		print(total, mid);
+		print(total, mid, tam);
+
+		cout << "Tiempo transcurrido = " << elapsed << endl;
 	}
 
 	int n;
@@ -100,10 +110,10 @@ void genNums(int* vec, int n, int mid) {
 }
 
 void mergeSort(int* vec, int mid, int cantxProc) {
-	sort(vec + mid*cantxProc, vec + (mid*cantxProc) + cantxProc);
+	sort(vec + mid * cantxProc, vec + (mid*cantxProc) + cantxProc);
 }
 
-void print(int* vec, int mid) {
+void print(int* vec, int mid, int tam) {
 	cout << " Imprimiendo vec de: " << mid << endl;
 	for (int i = 0; i < tam; i++) {
 		cout << vec[i] << ",";
@@ -146,10 +156,9 @@ void uso(string nombre_prog /* in */) {
    *		dato_salida: un dato de salida con un valor de argumento pasado por "línea de comandos".
    */
 void obt_args(
-	char*    argv[]        /* in  */,
-	int&     dato_salida  /* out */) {
+	char*    argv[]        /* in  */, int& tam) {
 
-	dato_salida = strtol(argv[1], NULL, 10); // se obtiene valor del argumento 1 pasado por "línea de comandos".
+	tam = strtol(argv[1], NULL, 10);
 
 #  ifdef DEBUG
 	cout << "dato_salida = " << dato_salida << endl;
